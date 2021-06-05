2c2191379f902f6f133c36b51acaa45033c51f17
Raheed Farooq <rfarooq@csquareonline.com>
2021-06-05 18:51:52 +0500

    Change Routes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,65 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
+          <div className="grid grid-cols-1 grid-rows-10 w-screen h-screen justify-between">
+            <header className="bg-prefered-black grid grid-cols-6 gap-0 items-center">
+              <div
+                className=" col-span-4 sm:col-span-2 md:col-span-1 "
+                style={{
+                  height: "inherit",
+                  overflow: "clip",
+                  objectFit: "cover",
+                }}
+              >
+                <img src={logo} style={{ width: "60%" }} />
+              </div>
+              <div
+                className=" col-span-1 sm:col-span-3 md:col-span-4 "
+                style={{ height: "inherit" }}
+              ></div>
+
+              {/* <button className="hidden md:block col-span-2 md:col-span-1 bg-prefered-shade2 mx-3 p-1">
+                Support Us
+              </button> */}
+              <div className="col-span-1 px-5">
+                {/* <button className="focus:outline-none w-12 md:w-16 h-12 md:h-12 float-right" onClick={()=>setSupport(!support)}>
+                <img
+                  class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+                  src="https://img.icons8.com/cotton/2x/like.png"
+                  alt=""
+                  ></img>
+              </button> */}
+              </div>
+
+              {support && (
+                <>
+                  <div
+                    class="origin-top-right absolute right-0 top-14 right-0 mr-0 md:right-5 mt-2 rounded-md shadow-lg bg-prefered-neon ring-1 ring-black ring-opacity-5 focus:outline-none max-w-xs h-auto	"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="menu-button"
+                    tabindex="-1"
+                  >
+                    <div class="" role="none">
+                      <img
+                        src="https://i.ytimg.com/vi/9XfkZlcG8KU/maxresdefault.jpg"
+                        className="rounded-lg border border-5"
+                      />
+                      {/* <a href="#" class="text-prefered-light block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a> */}
+                      <div className="text-center p-2 font-medium text-prefered-dark">
+                        If you found this site useful, please give us a Shout
+                        out on LinkedIn
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
+            </header>
+            <Home />
+          </div>
+        </Route>
+
+        <Route exact path="/v1">
           <div className="main absolute inset-0	bg-prefered-black">
             <div className="h-10p bg-prefered-dark"></div>
 
@@ -37,51 +96,6 @@ function App() {
             <Home />
             {/* <Docs /> */}
             {/* <Footer /> */}
-          </div>
-        </Route>
-        <Route exact path="/v3">
-          <div className="grid grid-cols-1 grid-rows-10 w-screen h-screen justify-between">
-            <header className="bg-prefered-black grid grid-cols-6 gap-0 items-center">
-              <div className=" col-span-4 sm:col-span-2 md:col-span-1 " style={{ height: "inherit",overflow: 'clip' , objectFit: 'cover' }}>
-                <img src={logo} style={{ width: "60%",  }} />
-              </div>
-              <div className=" col-span-1 sm:col-span-3 md:col-span-4 " style={{ height: "inherit" }}></div>
-
-              {/* <button className="hidden md:block col-span-2 md:col-span-1 bg-prefered-shade2 mx-3 p-1">
-                Support Us
-              </button> */}
-              <div className="col-span-1 px-5">
-
-              <button className="focus:outline-none w-12 md:w-16 h-12 md:h-12 float-right" onClick={()=>setSupport(!support)}>
-                <img
-                  class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
-                  src="https://img.icons8.com/cotton/2x/like.png"
-                  alt=""
-                  ></img>
-              </button>
-                  </div>
-
-              {support && (
-                <>
-                  <div
-                    class="origin-top-right absolute right-0 top-14 right-0 mr-0 md:right-5 mt-2 rounded-md shadow-lg bg-prefered-neon ring-1 ring-black ring-opacity-5 focus:outline-none max-w-xs h-auto	"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="menu-button"
-                    tabindex="-1"
-                  >
-                    <div class="" role="none">
-                      <img src="https://i.ytimg.com/vi/9XfkZlcG8KU/maxresdefault.jpg" className="rounded-lg border border-5" />
-                      {/* <a href="#" class="text-prefered-light block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a> */}
-                      <div className="text-center p-2 font-medium text-prefered-dark">
-                      If you found this site useful, please give us a Shout out on LinkedIn
-                      </div>
-                      </div>
-                  </div>
-                </>
-              )}
-            </header>
-            <Home />
           </div>
         </Route>
       </Switch>
